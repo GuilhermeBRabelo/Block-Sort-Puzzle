fef957b6496d6a3471223d1034c190c13b544631
GuilhermeBRabelo <guilherme.rabelo08@aluno.ifce.edu.br>
2025-02-03 19:48:34 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//função load, responsavel por carregar e criar os blocos
+
 function criaBloco() {
 
     var bttn = document.querySelector("#bttn");
@@ -6,10 +8,37 @@ function criaBloco() {
     containers.forEach(function(container) {
         for (var i = 0; i < 4; i++) {
             var bloco = document.createElement("div");
-            bloco.setAttribute("class", "blockCirculo");
-            // o erro tá aqui
+            
+            // aqui eu chamo a função random e defino via condição aritmetica qual será a classe do bloco
+            var imprime = geraId();
+            console.log(imprime);
+
+            if(imprime %2 == 0) {
+                bloco.setAttribute("class", "blockCirculo");
+            } else if (imprime %3 == 0) {
+                bloco.setAttribute("class", "blockQuadrado");
+            } else if (imprime %5 == 0) {
+                bloco.setAttribute("class", "blockTriangulo");
+            } else {
+                bloco.setAttribute("class", "blockX");
+            }
 
             container.appendChild(bloco);
         }
     });
+
 }
+
+//função responsavel por criar a aleatoriedade do ID
+
+function geraId() {
+    var id = Math.floor(Math.random() * 18);
+    return id;
+}
+
+//função responsável por mover os blocos
+
+//criar a função reset que irá apagar e criar novamente os quadrados via button
+
+//criar a função responsável por pontuar e modificar o score
+
