9d4d88beb693f4d3e52a78c13b70c2e557e53f38
GuilhermeBRabelo <guilherme.rabelo08@aluno.ifce.edu.br>
2025-02-12 11:17:49 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,29 +4,29 @@ var pontua = 0;
 
 //função responsavel por criar os blocos
 function criaBloco() {
-    var tuboVazio = document.querySelectorAll(".tubo");
-    var containers = document.querySelectorAll(".create");
+    var containers = document.querySelectorAll(".tubo");
+    var tuboVazio = document.querySelector("#tubo-vazio");
 
     tuboVazio.innerHTML = "";
-    
-    containers.forEach(function(container) {
+
+    containers.forEach(function (container) {
         for (let i = 0; i < 4; i++) {
             var bloco = document.createElement("div");
 
             bloco.setAttribute("draggable", "true");
-            
+
             var id = geraId();
             bloco.setAttribute("id", id);
-           
+
             let classe = parseInt(id / 4);
-            bloco.setAttribute("class", "bloco "+cores[classe]);
+            bloco.setAttribute("class", "bloco " + cores[classe]);
 
             container.appendChild(bloco);
 
+            bloco = moverBlocos();
         }
     });
-
-    moverBlocos();
+    
 }
 
 //função responsavel por criar a aleatoriedade do ID
@@ -35,10 +35,10 @@ function geraId() {
 
     do {
         aleatorio = Math.floor(Math.random() * 16);
-    }   while (vetorID.includes(aleatorio));
+    } while (vetorID.includes(aleatorio));
 
     vetorID.push(aleatorio);
-        
+
     return aleatorio;
 }
 
@@ -46,20 +46,45 @@ function geraId() {
 function moverBlocos() {
     var container = document.querySelectorAll(".tubo");
 
-    document.addEventListener("dragstart", function(e) {
+    document.addEventListener("dragstart", function (e) {
         e.target.classList.add("dragging");
     });
-    
-    document.addEventListener("dragend", function(e) {
+
+    document.addEventListener("dragend", function (e) {
         e.target.classList.remove("dragging");
         mudarPontua();
     });
-    
-    container.forEach(function(item) {
-        item.addEventListener("dragover", function(e) {
+
+    container.forEach(function (item) {
+        item.addEventListener("dragover", function (e) {
             const dragging = document.querySelector(".dragging");
             const applyAfter = getNewPosition(item, e.clientY);
+
+            if (applyAfter) {
+                applyAfter.insertAdjacentElement("afterend", dragging);
+            } else {
+                item.prepend(dragging);
+            }
+        });
+    });
     
+    
+    var containerVazio = document.querySelectorAll("#tubo-vazio");
+
+    document.addEventListener("dragstart", function (e) {
+        e.target.classList.add("dragging");
+    });
+
+    document.addEventListener("dragend", function (e) {
+        e.target.classList.remove("dragging");
+        mudarPontua();
+    });
+
+    containerVazio.forEach(function (item) {
+        item.addEventListener("dragover", function (e) {
+            const dragging = document.querySelector(".dragging");
+            const applyAfter = getNewPosition(item, e.clientY);
+
             if (applyAfter) {
                 applyAfter.insertAdjacentElement("afterend", dragging);
             } else {
@@ -70,16 +95,16 @@ function moverBlocos() {
 }
 
 // Função para calcular a nova posição ao mover o bloco
-function getNewPosition(column,posY){
+function getNewPosition(column, posY) {
     const cards = column.querySelectorAll(".item:not(.dragging)");
     let result;
 
-    for (let refer_card of cards){
+    for (let refer_card of cards) {
         const box = refer_card.getBoundingClientRect();
         const boxCenterY = box.y + box.height / 2;
 
-        if(posY >= boxCenterY)
-        result = refer_card;
+        if (posY >= boxCenterY)
+            result = refer_card;
     }
 
     return result;
@@ -89,7 +114,7 @@ function getNewPosition(column,posY){
 //Função responsável por pontuar e modificar o score
 function mudarPontua() {
     var contador = document.querySelector("#contador");
-    
+
     pontua += 10;
     contador.innerHTML = pontua;
 }
@@ -98,12 +123,12 @@ function mudarPontua() {
 //Função reset que irá apagar e criar novamente os quadrados via button
 function reset() {
     var containers = document.querySelectorAll(".tubo");
-    
-    containers.forEach(function(container) {
+
+    containers.forEach(function (container) {
         container.innerHTML = "";
     });
-    
+
     vetorID = [];
-     
+
     criaBloco();
 }
